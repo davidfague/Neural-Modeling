eb86504ea79e617db905c188da8b2f9ac033c295
David Fague <98128717+davidfague@users.noreply.github.com>
2023-06-20 15:17:59 -0500

    Add functions

--- Modules/cell_model.py
+++ Modules/cell_model.py
@@ -29,6 +29,13 @@ class CellModel:
         self.generate_sec_coords()
         self.seg_coords = self.calc_seg_coords()
 
+        self.__store_segments()
+        self.__set_spike_recorder()
+        #self.__store_synapses_list() #store and record synapses from the synapses_list used to initialize the cell
+        self.__get_segment_info__()
+        self.__insert_unused_channels()
+        self.__setup_recorders()
+
     # PRAGMA MARK: Section Generation
 
     # TODO: CHECK
@@ -155,8 +162,234 @@ class CellModel:
         seg_coords = {'p0': p0, 'p1': p1, 'pc': p05, 'r': r, 'dl': p1 - p0}
 
         return seg_coords
+        
+    # PRAGMA MARK: Spike Recording
 
+    # TODO: CHECK
 
+    def __set_spike_recorder(self, threshold: Optional = None):
+          if threshold is not None:
+              self.spike_threshold = threshold
+          if self.spike_threshold is None:
+              self.spikes = None
+          else:
+              vec = h.Vector()
+              nc = h.NetCon(self.soma(0.5)._ref_v, None, sec=self.soma)
+              nc.threshold = self.spike_threshold
+              nc.record(vec)
+              self.spikes = vec
+    
+    def get_spike_time(self, index: Union[np.ndarray, List[int], int, str] = 0) -> np.ndarray:
+          """
+          Return soma spike time of the cell by index (indices), ndarray (list of ndarray)
+          Parameters
+          index: index of the cell to retrieve the spikes from
+          """
+          if self.spike_threshold is None:
+              raise ValueError("Spike recorder was not set up.")
+          if type(index) is str and index == 'all':
+              index = range(self.ncell)
+          if not hasattr(index, '__len__'):
+              spk = self.spikes.as_numpy().copy()
+          else:
+              index = np.asarray(index).ravel()
+              spk = np.array([self.spikes.as_numpy().copy() for i in index], dtype=object)
+          return spk
+    
+    # PRAGMA MARK: Segment Recording
+
+    # TODO: CHECK
+
+    def __store_segments(self):
+        self.segments = []
+        for sec in self.all:
+            for seg in sec:
+                self.segments.append(seg)
+    
+    def __insert_unused_channels(self):
+      channels = [('NaTa_t', 'gNaTa_t_NaTa_t', 'gNaTa_tbar'),
+                  ('Ca_LVAst', 'ica_Ca_LVAst', 'gCa_LVAstbar'),
+                  ('Ca_HVA', 'ica_Ca_HVA', 'gCa_HVAbar'),
+                  ('Ih', 'ihcn_Ih', 'gIhbar')]
+      for channel, attr, conductance in channels:
+          for sec in self.all:
+              if not hasattr(sec(0.5), attr):
+                  sec.insert(channel)
+                  for seg in sec:
+                      setattr(getattr(seg, channel), conductance, 0)
+                  # print(channel, sec) # empty sections
+
+    def __setup_recorders(self):
+        self.gNaTa_T = Recorder(obj_list=self.segments, var_name='gNaTa_t_NaTa_t')
+        self.ina = Recorder(obj_list=self.segments, var_name='ina_NaTa_t')
+        self.ical = Recorder(obj_list=self.segments, var_name='ica_Ca_LVAst')
+        self.icah = Recorder(obj_list=self.segments, var_name='ica_Ca_HVA')
+        self.ih = Recorder(obj_list=self.segments, var_name='ihcn_Ih')
+        self.Vm = Recorder(obj_list=self.segments)
+
+    def __get_segment_info__(self):
+          self.seg_info = []
+          k = 0
+          j = 0
+          for sec in self.all:
+              sec_type = sec.name().split('.')[1][:4]
+              for i, seg in enumerate(sec):
+                  self.seg_info.append({ #update to have consistent naming scheme (will then need to debug plotting functions too, but should be easy)
+                      'seg': seg,
+                      'seg_id': j,
+                      'Beginning X Coord': self.seg_coords['p0'][i][0],
+                      'Beginning Y Coord': self.seg_coords['p0'][i][1],
+                      'Beginning Z Coord': self.seg_coords['p0'][i][2],
+                      'Center X Coord': self.seg_coords['pc'][i][0],
+                      'Center Y Coord': self.seg_coords['pc'][i][1],
+                      'Center Z Coord': self.seg_coords['pc'][i][2],
+                      'End X Coord': self.seg_coords['p1'][i][0],
+                      'End Y Coord': self.seg_coords['p1'][i][1],
+                      'End Z Coord': self.seg_coords['p1'][i][2],
+                      'seg diam': seg.diam,
+                      'bmtk_id': k,
+                      'x': seg.x,
+                      'sec': seg.sec,
+                      'Type': sec_type,
+                      'Sec ID': int(sec.name().split('[')[2].split(']')[0]),
+                      'sec diam': sec.diam,
+                      'sec nseg': seg.sec.nseg,
+                      'Ra': seg.sec.Ra,
+                      'seg_L': sec.L/sec.nseg,
+                      'seg_SA': (sec.L/sec.nseg)*(np.pi*seg.diam),
+                  })
+                  j += 1
+              k += 1
+          return self.__get_parent_segment_ids()
+    def __get_parent_segment_ids(self):
+          for seg in self.seg_info:
+              seg['parent_seg_id'] = None
+          pseg_ids = []
+          for i, seg in enumerate(self.seg_info):
+              idx = int(np.floor(seg['x'] * seg['sec nseg']))
+              if idx != 0:
+                  pseg_id = i-1
+              else:
+                  pseg = seg['seg'].sec.parentseg()
+                  if pseg is None:
+                      pseg_id = None
+                  else:
+                      psec = pseg.sec
+                      nseg = psec.nseg
+                      pidx = int(np.floor(pseg.x * nseg))
+                      if pseg.x == 1.:
+                          pidx -= 1
+                      try:
+                          pseg_id = next(idx for idx, info in enumerate(self.seg_info) if info['seg'] == psec((pidx + .5) / nseg))
+                      except StopIteration:
+                          pseg_id = "Segment not in segments"
+                  self.seg_info[i]['parent_seg_id'] = pseg_id
+              # pseg_ids.append(pseg_id)
+          return self.__get_segment_elec_dist()
+    
+    def __get_segment_elec_dist(self):
+          for seg in self.seg_info:
+              seg['seg_elec_info'] = {}
+          freqs = {'delta': 1, 'theta': 4, 'alpha': 8, 'beta': 12, 'gamma': 30}
+    
+          soma_passive_imp = h.Impedance()
+          soma_active_imp = h.Impedance()
+          nexus_passive_imp = h.Impedance()
+          nexus_active_imp = h.Impedance()
+          try:
+              soma_passive_imp.loc(self.hobj.soma[0](0.5))
+              soma_active_imp.loc(self.hobj.soma[0](0.5))
+          except:
+              try:
+                  soma_passive_imp.loc(self.soma[0](0.5))
+                  soma_active_imp.loc(self.soma[0](0.5))
+              except:
+                  try:
+                      soma_passive_imp.loc(self.soma(0.5))
+                      soma_active_imp.loc(self.soma(0.5))
+                  except:
+                      raise AttributeError("Could not locate soma for impedance calculation")
+          try:
+              nexus_passive_imp.loc(self.hobj.apic[0](0.99))
+              nexus_active_imp.loc(self.hobj.apic[0](0.99))
+          except:
+              try:
+                  nexus_passive_imp.loc(self.apic[0](0.99))
+                  nexus_active_imp.loc(self.apic[0](0.99))
+              except:
+                  try:
+                      nexus_passive_imp.loc(self.apic(0.99))
+                      nexus_active_imp.loc(self.apic(0.99))
+                  except:
+                      raise AttributeError("Could not locate the nexus for impedance calculation")
+    
+          for freq_name, freq_hz in freqs.items():
+              soma_passive_imp.compute(freq_hz + 1 / 9e9, 0) #passive from soma
+              soma_active_imp.compute(freq_hz + 1 / 9e9, 1) #active from soma
+              nexus_passive_imp.compute(freq_hz + 1 / 9e9, 0) #passive from nexus
+              nexus_active_imp.compute(freq_hz + 1 / 9e9, 1) #active from nexus
+              for i, seg in enumerate(self.segments):
+                  elec_dist_info = {
+                      'active_soma': soma_active_imp.ratio(seg.sec(seg.x)),
+                      'active_nexus': nexus_active_imp.ratio(seg.sec(seg.x)),
+                      'passive_soma': soma_passive_imp.ratio(seg.sec(seg.x)),
+                      'passive_nexus': nexus_passive_imp.ratio(seg.sec(seg.x))
+                  }
+                  self.seg_info[i]['seg_elec_info'][freq_name] = elec_dist_info
+          return self.__calculate_netcons_per_seg()
+    
+    def __calculate_netcons_per_seg(self):
+          NetCon_per_seg = [0] * len(self.seg_info)
+          inh_NetCon_per_seg = [0] * len(self.seg_info)
+          exc_NetCon_per_seg = [0] * len(self.seg_info)
+    
+          v_rest = -60 #used to determine exc/inh may adjust or automate
+    
+          # calculate number of synapses for each segment (may want to divide by segment length afterward to get synpatic density)
+          for netcon in self.netcons_list:
+              syn = netcon.syn()
+              syn_type=syn.hname().split('[')[0]
+              if (syn in self.synapses_list) or syn in self.synapse:
+                  syn_seg_id = self.seg_info.index(next((s for s in self.seg_info if s['seg'] == syn.get_segment()), None))
+                  seg_dict = self.seg_info[syn_seg_id]
+                  if syn in seg_dict['seg'].point_processes():
+                      NetCon_per_seg[syn_seg_id] += 1 # get synapses per segment
+                      if syn_type == 'pyr2pyr':
+                          exc_NetCon_per_seg[syn_seg_id] += 1
+                      elif syn_type == 'int2pyr':
+                          inh_NetCon_per_seg[syn_seg_id] += 1
+                      elif 'AMPA_NMDA' in syn_type:
+                          exc_NetCon_per_seg[syn_seg_id] += 1
+                      elif 'GABA_AB' in syn_type:
+                          inh_NetCon_per_seg[syn_seg_id] += 1
+                      elif syn.e > v_rest:
+                          exc_NetCon_per_seg[syn_seg_id] += 1
+                      else:
+                          inh_NetCon_per_seg[syn_seg_id] += 1
+                  else:
+                      print("Warning: synapse not in designated segment's point processes")
+              else:
+                  print("Warning: potentially deleted synapse:","|NetCon obj:",netcon,"|Synapse obj:",syn,"the NetCon's synapse is not in synapses_list. Check corresponding original cell's NetCon for location, etc.")
+    
+          for i, seg in enumerate(self.seg_info):
+              seg['netcons_per_seg'] = {
+                  'exc': exc_NetCon_per_seg[i],
+                  'inh': inh_NetCon_per_seg[i],
+                  'total': NetCon_per_seg[i]
+              }
+              seg['netcon_density_per_seg'] = {
+                  'exc': exc_NetCon_per_seg[i]/seg['seg_L'],
+                  'inh': inh_NetCon_per_seg[i]/seg['seg_L'],
+                  'total': NetCon_per_seg[i]/seg['seg_L']
+              }
+              seg['netcon_SA_density_per_seg'] = {
+                  'exc': exc_NetCon_per_seg[i]/seg['seg_SA'],
+                  'inh': inh_NetCon_per_seg[i]/seg['seg_SA'],
+                  'total': NetCon_per_seg[i]/seg['seg_SA']
+              }
+    
+          return
+        
     # PRAGMA MARK: Utils
 
     # TODO: CHECK
