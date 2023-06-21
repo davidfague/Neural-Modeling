95e3a626b75f633ef645a1aba87350b25b6e893e
David Fague <98128717+davidfague@users.noreply.github.com>
2023-06-21 11:44:43 -0500

    Create simulation.py

---
+++ Modules/simulation.py
@@ -0,0 +1,18 @@
+# Work in progress
+# module for running simulation
+import time
+
+class Simulation():
+  def __init__():
+    pass
+
+  def run(self):
+    self.start_timer()
+    h.run()
+    self.end_timer()
+    
+  def start_timer(self):
+    pass
+    
+  def end_timer(self):
+    pass
