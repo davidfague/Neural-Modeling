929e746a767b2122d3e9fb1797cec20b25dada92
David Fague <98128717+davidfague@users.noreply.github.com>
2023-06-21 11:49:24 -0500

    Update simulation.py

--- Modules/simulation.py
+++ Modules/simulation.py
@@ -3,14 +3,22 @@
 import time
 
 class Simulation():
-  def __init__():
-    pass
+  def __init__(self, filename: str = None, record_ecp: bool = False):
+    self.filename=filename
 
   def run(self):
     self.start_timer()
     h.run()
     self.end_timer()
-    
+    if filename is not None:
+      self.write_data_to_file():
+
+  def write_data_to_file(self):
+    pass
+  
+  def record_ecp(self):
+    pass
+  
   def start_timer(self):
     pass
     
