8b30dcb8c44e0a31c85a078462939a566605cda9
David Fague <98128717+davidfague@users.noreply.github.com>
2023-06-23 13:28:00 -0500

    add syn_params

--- Modules/functional_group.py
+++ Modules/functional_group.py
@@ -39,7 +39,7 @@ def generate_excitatory_functional_groups(all_segments: list, all_segments_cente
 										  functional_group_span: float, cluster_span: float, 
 										  gmax_dist: object, mean_fr_dist: object, 
 										  spike_generator: SpikeGenerator, synapse_generator: SynapseGenerator,
-										  t: np.ndarray, record: bool = False) -> list:
+										  t: np.ndarray, record: bool = False, syn_params: dict = None) -> list:
 
 	functional_groups = []
 
@@ -66,7 +66,7 @@ def generate_excitatory_functional_groups(all_segments: list, all_segments_cente
 			# Add synapses to to the cluster
 			cluster.synapses = synapse_generator.add_synapses(segments = cluster.segments, probs = cluster.len_per_segment,
 							 								  gmax = gmax_dist, syn_mod = 'AMPA_NMDA',
-															  number_of_synapses = synapses_per_cluster, record = record)
+															  number_of_synapses = synapses_per_cluster, record = record,syn_params=syn_params)
 
 			# Generate spikes common to each synapse within synaptic cluster
 			mean_fr = spike_generator.get_mean_fr(mean_fr_dist)
@@ -88,7 +88,7 @@ def generate_inhibitory_functional_groups(cell: object, all_segments: list, all_
 										  spike_generator: SpikeGenerator, synapse_generator: SynapseGenerator,
 										  t: np.ndarray, f_group_name_prefix: str, 
 										  spike_trains_to_delay: list, fr_time_shift: int,
-										  record: bool = False) -> list:
+										  record: bool = False,syn_params: dict = None) -> list:
 	functional_groups = []
 
 	rnd = np.random.RandomState(10)
@@ -120,7 +120,7 @@ def generate_inhibitory_functional_groups(cell: object, all_segments: list, all_
 			# Add synapses to to the cluster
 			cluster.synapses = synapse_generator.add_synapses(segments = cluster.segments, probs = cluster.len_per_segment,
 							 								  gmax = gmax_dist, syn_mod = 'GABA_AB',
-															  number_of_synapses = synapses_per_cluster, record = record)
+															  number_of_synapses = synapses_per_cluster, record = record,syn_params=syn_params)
 
 			# Generate spikes common to each synapse within synaptic cluster
 			mean_fr = spike_generator.get_mean_fr(mean_fr_dist)
