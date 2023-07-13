d91d78bc63068288813aed9a2d45080830da989f
David Fague <98128717+davidfague@users.noreply.github.com>
2023-07-13 14:46:44 -0500

    Create clustering.py

---
+++ Modules/clustering.py
@@ -0,0 +1,64 @@
+import networkx as nx
+from sklearn.cluster import KMeans
+from neuron_reduce.reducing_methods import (_get_subtree_biophysical_properties, find_space_const_in_cm)
+
+# Create a graph from the segments
+def create_graph(segments):
+    G = nx.Graph()
+    for segment in segments:
+        G.add_edge(*segment)
+    return G
+
+def calc_elec_distance(seg1, seg2, frequency):
+    # Initialize a queue for BFS with the start segment
+    # Add half of the electrotonic length of seg1 to the starting distance
+    cm, rm, ra, e_pas, q = _get_subtree_biophysical_properties(h.SectionRef(sec=seg1), frequency)
+    cable_space_const_in_cm = find_space_const_in_cm(seg1.diam/10000, rm, ra)
+    cable_elec_L = seg1.L/(cable_space_const_in_cm*10000)
+    start_dist = cable_elec_L / 2
+
+    queue = [(seg1, start_dist)]  
+    visited = {seg1}
+
+    while queue:
+        current_seg, dist = queue.pop(0)
+        if current_seg == seg2:
+            # Subtract half of the electrotonic length of seg2 from the final distance
+            cm, rm, ra, e_pas, q = _get_subtree_biophysical_properties(h.SectionRef(sec=seg2), frequency)
+            cable_space_const_in_cm = find_space_const_in_cm(seg2.diam/10000, rm, ra)
+            cable_elec_L = seg2.L/(cable_space_const_in_cm*10000)
+            end_dist = cable_elec_L / 2
+
+            return dist - end_dist
+        for adj_seg in current_seg.adj_segs:
+            if adj_seg not in visited:
+                # get biophysical properties
+                cm, rm, ra, e_pas, q = _get_subtree_biophysical_properties(h.SectionRef(sec=adj_seg), frequency)
+                # calculate the space constant
+                cable_space_const_in_cm = find_space_const_in_cm(adj_seg(0.5).diam/10000, rm, ra)
+                # calculate the electrotonic length of the segment
+                cable_elec_L = adj_seg.L/(cable_space_const_in_cm*10000)
+                # add the segment to the queue with the updated distance
+                queue.append((adj_seg, dist+cable_elec_L))
+                visited.add(adj_seg)
+    raise ValueError("No path between seg1 and seg2")
+
+
+# calculate the electrotonic length of the cable
+cm, rm, ra, e_pas, q = _get_subtree_biophysical_properties(h.SectionRef(sec=section_to_expand), frequency)
+cable_space_const_in_cm = find_space_const_in_cm(section_to_expand(0.5).diam/10000, rm, ra)
+cable_elec_L = section_to_expand.L/(cable_space_const_in_cm*10000)
+
+# Group synapses into clusters based on their segment's electrotonic distances
+def cluster_synapses(synapses, n_clusters):
+    segments = list(set([synapse.seg for synapse in synapses]))
+    G = create_graph(segments)
+    # Create a list of distances
+    distances = [[calc_elec_distance(segment1, segment2, frequency) for segment2 in segments] for segment1 in segments]
+    # Perform the clustering
+    kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(distances)
+    # Create a dictionary to map segments to clusters
+    segment_cluster_map = {segment: cluster for segment, cluster in zip(segments, kmeans.labels_)}
+    # Map each synapse to its segment's cluster
+    synapse_cluster_map = {synapse: segment_cluster_map[synapse.seg] for synapse in synapses}
+    return synapse_cluster_map
