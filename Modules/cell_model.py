fd346779d03f17edf20b5e8be9c682533bbda75e
David Fague <98128717+davidfague@users.noreply.github.com>
2023-07-13 17:18:30 -0500

    Update cell_model.py

--- Modules/cell_model.py
+++ Modules/cell_model.py
@@ -271,7 +271,10 @@ class CellModel:
                     except StopIteration:
                         pseg_id = None
                           
-            self.seg_info[i]['pseg_index'] = pseg_id.astype(int)
+            if pseg_id is not None:
+                self.seg_info[i]['pseg_index'] = pseg_id.astype(int)
+            else:
+                self.seg_info[i]['pseg_index'] = pseg_id
     
     def recompute_segment_elec_distance(self, segment, seg_name) -> None:
         if not all('seg_elec_distance' in seg for seg in self.seg_info):
