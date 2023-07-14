c7bb3c7c46cad5b5918c1de559018fed0f14573f
David Fague <98128717+davidfague@users.noreply.github.com>
2023-07-14 15:52:47 -0500

    change 3d_morph arg

--- Modules/complex_cell.py
+++ Modules/complex_cell.py
@@ -1,17 +1,17 @@
 from neuron import h
 
-def build_L5_cell(cell_folder, biophys = 'L5PCbiophys3.hoc', 3d_morph = 'import3d.hoc'):
+def build_L5_cell(cell_folder, biophys = 'L5PCbiophys3.hoc', 3d_morph = 'cell1.asc'):
     # Load biophysics
     h.load_file(cell_folder + biophys)
 
     # Load morphology
-    h.load_file(3d_morph)
+    h.load_file("import3d.hoc")
 
     # Load builder
     h.load_file(cell_folder + 'L5PCtemplate.hoc')
 
     # Build complex_cell object
-    complex_cell = h.L5PCtemplate(cell_folder + 'cell1.asc')
+    complex_cell = h.L5PCtemplate(cell_folder + 3d_morph)
 
     return complex_cell
 
