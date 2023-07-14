8108c9822246c383f30fa0f285475b31b72edafd
David Fague <98128717+davidfague@users.noreply.github.com>
2023-07-14 16:27:29 -0500

    try template arg

--- Modules/complex_cell.py
+++ Modules/complex_cell.py
@@ -1,6 +1,6 @@
 from neuron import h
 
-def build_L5_cell(cell_folder, biophys = 'L5PCbiophys3.hoc', morph = 'cell1.asc'):
+def build_L5_cell(cell_folder, biophys = 'L5PCbiophys3.hoc', morph = 'cell1.asc', template = 'L5PCtemplate.hoc'):
     # Load biophysics
     h.load_file(cell_folder + biophys)
 
@@ -8,10 +8,11 @@ def build_L5_cell(cell_folder, biophys = 'L5PCbiophys3.hoc', morph = 'cell1.asc'
     h.load_file("import3d.hoc")
 
     # Load builder
-    h.load_file(cell_folder + 'L5PCtemplate.hoc')
+    h.load_file(cell_folder + template)
 
     # Build complex_cell object
-    complex_cell = h.L5PCtemplate(cell_folder + morph)
+    eval("complex_cell = h." + template.split('.')[0] + '(cell_folder + morph)')
+    #complex_cell = h.L5PCtemplate(cell_folder + morph)
 
     return complex_cell
 
