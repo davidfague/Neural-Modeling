12479c642bf7da093c8fd8de27726a3ae0c6a971
David Fague <98128717+davidfague@users.noreply.github.com>
2023-07-14 22:37:36 -0500

    add tufts, basals, _nbranch, and find_distal_sections

--- Modules/cell_model.py
+++ Modules/cell_model.py
@@ -40,6 +40,13 @@ class CellModel:
         self.spike_threshold = spike_threshold
         self.injection = []
         self.var_names = var_names  # variables to be recorded
+        self.tufts=self.find_distal_sections(cell, 'apic')
+		self.basals=self.find_distal_sections(cell, 'dend')
+        if len(tufts) == 1:
+            self._nbranch = len(self.tufts) + len(self.basals) # trunk is branch
+        else:
+            self._nbranch = len(self.tufts) - 1 + len(self.basals) # trunk is not branch
+		cell._nbranch=len(tufts)
         
         # Angles and rotations that were used to branch the cell
         # Store to use for geometry file generation
@@ -511,3 +518,27 @@ class CellModel:
             'adjacent_segments': []
         }
         return info
+        
+        def find_distal_sections(self, region=str):
+        	'''
+        	Finds all terminal sections then gathers terminal apical sections that are greater than 800 microns from the soma in path length
+        	'''
+        	# find distal tuft sections:
+        	parent_sections=[]
+        	for sec in cell.all: # find non-terminal sections
+        		if sec.parentseg() is not None:
+        			if sec.parentseg().sec not in parent_sections:
+        				parent_sections.append(sec.parentseg().sec)
+        	terminal_sections=[]
+        	for sec in getattr(cell,region):  # check if the section is a terminal section and if it is apical tuft
+        		# print(h.distance(sec(0.5)))
+        		if region=='apic':
+        			if (sec not in parent_sections) and (h.distance(cell.soma[0](0.5),sec(0.5)) > 800):
+        				terminal_sections.append(sec)
+        		else:
+        			if (sec not in parent_sections):
+        				terminal_sections.append(sec)
+        
+        			# print(sec, 'is a terminal section of the tuft'
+        
+        	return terminal_sections
