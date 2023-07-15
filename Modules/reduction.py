e146ea9ac1cb95b428fe57f7cbe179c6b0e159fc
David Fague <98128717+davidfague@users.noreply.github.com>
2023-07-14 23:17:52 -0500

    add cell_model args

--- Modules/reduction.py
+++ Modules/reduction.py
@@ -25,28 +25,37 @@ class Reductor():
 			elif method is not None:
 				raise NotImplementedError
 
-	def reduce_cell_func(self, complex_cell, reduce_cell: bool=False, synapses_list:list=None, netcons_list: list=None, reduction_frequency=0, expand_cable: bool =False, choose_branches: list=None):
-		if reduce_cell:
-			self.reduced_cell, synapses_list, netcons_list, txt_nr = subtree_reductor(complex_cell, synapses_list, netcons_list, reduction_frequency, return_seg_to_seg=True)
-			if expand_cable:
-				sections_to_expand = [self.reduced_cell.hoc_model.apic[0]]
-				furcations_x = [0.289004]
-				nbranches = [choose_branches]
-				self.reduced_dendritic_cell, synapses_list, netcons_list, txt_ce = cable_expander(self.reduced_cell, sections_to_expand, furcations_x, nbranches,
-																							 synapses_list, netcons_list, reduction_frequency, return_seg_to_seg=True)
-				cell = CellModel(self.reduced_dendritic_cell, synapses_list=synapses_list, netcons_list=netcons_list, spike_threshold=10)
-				print(len(cell.tufts), "terminal tuft branches in reduced_dendritic_cell")
-			else:
-				self.reduced_cell.all = []
-				for sec in [self.reduced_cell.soma, self.reduced_cell.apic] + self.reduced_cell.dend + self.reduced_cell.axon:
-					self.reduced_cell.all.append(sec)
-				cell = CellModel(self.reduced_cell, synapses_list=synapses_list, netcons_list=netcons_list, spike_threshold=10)
-				print(len(cell.tufts), "terminal tuft branches in NR reduced_cell")
-			return cell
-		else:
-			cell = CellModel( complex_cell, synapses_list=synapses_list, netcons_list=netcons_list, spike_threshold=10)
-			print(len(cell.tufts), "terminal tuft branches in complex_cell")
-			return cell
+	def reduce_cell_func(self, complex_cell, reduce_cell: bool=False, synapses_list:list=None, netcons_list: list=None, 
+		                     spike_trains=None, spike_threshold: int=10, random_state=None, var_names=None, 
+		                     reduction_frequency=0, expand_cable: bool =False, choose_branches: list=None):
+		    if reduce_cell:
+		        self.reduced_cell, synapses_list, netcons_list, txt_nr = subtree_reductor(complex_cell, synapses_list, netcons_list, reduction_frequency, return_seg_to_seg=True)
+		        if expand_cable:
+		            sections_to_expand = [self.reduced_cell.hoc_model.apic[0]]
+		            furcations_x = [0.289004]
+		            nbranches = [choose_branches]
+		            self.reduced_dendritic_cell, synapses_list, netcons_list, txt_ce = cable_expander(self.reduced_cell, sections_to_expand, furcations_x, nbranches,
+		                                                                                             synapses_list, netcons_list, reduction_frequency, return_seg_to_seg=True)
+		            cell = CellModel(hoc_model=self.reduced_dendritic_cell, synapses=synapses_list, netcons=netcons_list, 
+		                             spike_trains=spike_trains, spike_threshold=spike_threshold, random_state=random_state,
+		                             var_names=var_names)
+		            print(len(cell.tufts), "terminal tuft branches in reduced_dendritic_cell")
+		        else:
+		            self.reduced_cell.all = []
+		            for sec in [self.reduced_cell.soma, self.reduced_cell.apic] + self.reduced_cell.dend + self.reduced_cell.axon:
+		                self.reduced_cell.all.append(sec)
+		            cell = CellModel(hoc_model=self.reduced_cell, synapses=synapses_list, netcons=netcons_list, 
+		                             spike_trains=spike_trains, spike_threshold=spike_threshold, random_state=random_state,
+		                             var_names=var_names)
+		            print(len(cell.tufts), "terminal tuft branches in NR reduced_cell")
+		        return cell
+		    else:
+		        cell = CellModel(hoc_model=complex_cell, synapses=synapses_list, netcons=netcons_list, 
+		                         spike_trains=spike_trains, spike_threshold=spike_threshold, random_state=random_state,
+		                         var_names=var_names)
+		        print(len(cell.tufts), "terminal tuft branches in complex_cell")
+		        return cell
+
 
 	def find_space_const_in_cm(self, diameter, rm, ra):
 		''' returns space constant (lambda) in cm, according to: space_const = sqrt(rm/(ri+r0)) '''
