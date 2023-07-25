c1d265d7967a863d9c73227e75b9703f0f04fb3b
David Fague <98128717+davidfague@users.noreply.github.com>
2023-07-25 15:53:16 -0500

    fix integration with existing synapses

--- Modules/synapse.py
+++ Modules/synapse.py
@@ -93,7 +93,7 @@ class CurrentInjection:
 
 class Synapse:
 
-    def __init__(self, segment: nrn.Segment = None, syn_mod: str = None, gmax: float = 0.01, record: bool = False, 
+    def __init__(self, segment: nrn.Segment = None, syn_mod: str = None, gmax: float = None, record: bool = False, 
                  syn_params: dict = None, syn_obj: object = None, vector_length: int = None):
         '''
         Parameters:
@@ -122,9 +122,14 @@ class Synapse:
         elif syn_obj:
             self.segment = syn_obj.get_segment()
         else:
-            raise(ValueError("Need to pass either existing neuron synapse object or argument to create new synapse."))
+            raise(ValueError("Need to pass either existing neuron synapse object or segment to create new synapse."))
         self.syn_type = syn_mod
-        self.gmax = gmax
+        if (gmax is None) and (syn_obj is not None):
+          self.gmax = self.get_gmax_from_syn_obj()
+        elif gmax is not None:
+          self.gmax = gmax
+        else:
+          raise(ImplementationError("Need to pass either existing neuron synapse object or gmax to create new synapse."))
         self.gmax_var = None # Variable name of maximum conductance (uS)
         self.syn_params = syn_params
         self.synapse_neuron_obj = syn_obj
@@ -132,7 +137,6 @@ class Synapse:
             self.syn_type = str(self.synapse_neuron_obj).split('[')[0]
         self.rec_vec = []  # List of vectors for recording
         self.set_params_based_on_synapse_mod(self.syn_type)
-        self.gmax = self.get_gmax()
         self.setup(record, vector_length)
         self.ncs = []
 
@@ -193,9 +197,8 @@ class Synapse:
         else:
             setattr(self.synapse_neuron_obj, self.gmax_var, self.gmax)
 
-    def get_gmax(self) -> None:
+    def get_gmax_from_syn_obj(self) -> None:
         gmax = getattr(self.synapse_neuron_obj, self.gmax_var)
-        self.gmax = gmax
         return gmax
     
     def setup_recorder(self, vector_length) -> None:
