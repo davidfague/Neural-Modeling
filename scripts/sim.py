09b103feeac39b55a07f96d29fa24275339ef199
Vladimir Omelyusik <vsomelyusik@gmail.com>
2023-10-26 14:02:06 -0500

    merge confl

--- scripts/sim.py
+++ scripts/sim.py
@@ -9,17 +9,18 @@ import numpy as np
 
 if __name__ == "__main__":
     sim = Simulation(SkeletonCell.Hay)
+    sim.submit_job(HayParameters(sim_name = "ben", h_tstop = 2000))
     
     # Looping over the range for exc_gmax_mean
-    for exc_gmax_mean in np.arange(0.15, 0.46, 0.05):  # Assuming a step of 0.05, modify as needed
-        for clip_upper_bound in np.arange(0.1, 1.1, 0.1):  # Assuming a step of 0.1, modify as needed
-            exc_gmax_clip = (0, clip_upper_bound)
+    # for exc_gmax_mean in np.arange(0.15, 0.46, 0.05):  # Assuming a step of 0.05, modify as needed
+    #     for clip_upper_bound in np.arange(0.1, 1.1, 0.1):  # Assuming a step of 0.1, modify as needed
+    #         exc_gmax_clip = (0, clip_upper_bound)
             
-            # Now you can use these values in your simulation parameters
-            sim.submit_job(HayParameters(
-                sim_name=f"HAY_sim_mean_{exc_gmax_mean}_clip_{clip_upper_bound}",
-                exc_gmax_mean_0=exc_gmax_mean,
-                exc_gmax_clip=exc_gmax_clip
-            ))
+    #         # Now you can use these values in your simulation parameters
+    #         sim.submit_job(HayParameters(
+    #             sim_name=f"HAY_sim_mean_{exc_gmax_mean}_clip_{clip_upper_bound}",
+    #             exc_gmax_mean_0=exc_gmax_mean,
+    #             exc_gmax_clip=exc_gmax_clip
+    #         ))
 
     sim.run()
