d87473a91b2642b1f81c0ab52507d22fa18e0ed5
davidfague <davidfague@live.com>
2023-10-25 20:36:30 -0500

    fix indent

--- Modules/reduction.py
+++ Modules/reduction.py
@@ -40,9 +40,9 @@ class Reductor():
 				# Get the mapping of nrn.Synapse to NetCon
 				syn_to_netcon = get_syn_to_netcons(netcons_list)
 	
-# Convert nrn.Synapse objects back to Synapse class and append netcons
-synapses_list = []
-synapses_without_netcons = []
+        # Convert nrn.Synapse objects back to Synapse class and append netcons
+        synapses_list = []
+        synapses_without_netcons = []
 
 				for nrn_syn in nrn_synapses_list:
 					if nrn_syn in syn_to_netcon.keys():
