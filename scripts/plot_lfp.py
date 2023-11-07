6800d069094a991d52b501863e365ce70175d593
Vladimir Omelyusik <vsomelyusik@gmail.com>
2023-11-07 00:36:51 -0600

    make plot_lfp compatible with other scripts

--- scripts/plot_lfp.py
+++ scripts/plot_lfp.py
@@ -2,27 +2,29 @@ import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 import sys, os
-
 sys.path.append("../")
-from Modules.plotting_utils import plot_simulation_results
-from cell_inference.config import params
-import pickle
-from cell_inference.utils.plotting.plot_results import plot_lfp_heatmap, plot_lfp_traces
 
-if __name__ == "__main__":
-	# Parse cl args
-	if len(sys.argv) != 3:
-		raise RuntimeError("usage: python file -f file")
-	else:
-		output_folder = sys.argv[sys.argv.index("-f") + 1]
+from Modules.plotting_utils import plot_simulation_results
+from Modules.constants import SimulationParameters
+from Modules.logger import Logger
+
+from cell_inference.utils.plotting.plot_results import plot_lfp_heatmap, plot_lfp_traces
+from cell_inference.config import params
+from multiprocessing import Pool, cpu_count
+
+import pickle
+
+def wrap_plot_lfp(parameters: SimulationParameters):
+
+	step_size = int(parameters.save_every_ms / parameters.h_dt) # Timestamps
+	steps = range(step_size, int(parameters.h_tstop / parameters.h_dt) + 1, step_size) # Timestamps
 
 	t = []
 	Vm = []
 	lfp = []
-	steps = [10000, 20000]
-	step_size = 10000
+
 	for step in steps:
-		dirname = os.path.join(output_folder, f"saved_at_step_{step}")
+		dirname = os.path.join(parameters.path, f"saved_at_step_{step}")
 		with h5py.File(os.path.join(dirname, "Vm_report.h5")) as file:
 			Vm.append(np.array(file["report"]["biophysical"]["data"])[:, :step_size])
 		with h5py.File(os.path.join(dirname, "lfp.h5")) as file:
@@ -34,7 +36,7 @@ if __name__ == "__main__":
 	lfp = np.vstack(lfp)
 	t = np.hstack(t) # (ms)
 
-	with open(os.path.join(output_folder, "seg_indexes.pickle"), "rb") as file:
+	with open(os.path.join(parameters.path, "seg_indexes.pickle"), "rb") as file:
 		seg_indexes = pickle.load(file)
 	
 	loc_param = [0., 0., 45., 0., 1., 0.]
@@ -56,4 +58,31 @@ if __name__ == "__main__":
 		plot_lfp_traces, 
 		vlim = [-0.023, 0.023],
 		show = False,
-		save_dir = output_folder)
+		save_dir = parameters.path)
+
+
+if __name__ == "__main__":
+	# Parse cl args
+	if len(sys.argv) != 3:
+		raise RuntimeError("usage: python file -f folder")
+	else:
+		sim_folder = sys.argv[sys.argv.index("-f") + 1]
+
+	logger = Logger(None)
+	
+	# Retrieve all jobs
+	jobs = []
+	for job in os.listdir(sim_folder):
+		if job.startswith("."): continue
+		with open(os.path.join(sim_folder, job, "parameters.pickle"), "rb") as parameters:
+			jobs.append(pickle.load(parameters))
+
+	logger.log(f"Total number of jobs found: {len(jobs)}")
+	logger.log(f"Total number of proccessors: {cpu_count()}")
+
+	pool = Pool(processes = len(jobs))
+	pool.map(wrap_plot_lfp, jobs)
+	pool.close()
+	pool.join()
+
+	
