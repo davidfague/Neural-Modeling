4af3cd62c1bde535d4c6edfa27d1436a45e33d82
davidfague <davidfague@live.com>
2024-05-17 13:46:51 -0500

    compartmentalize simulate(); add set_all_recorders from cell_builder; add/depracate EPSP simulations

--- Modules/simulation.py
+++ Modules/simulation.py
@@ -81,7 +81,7 @@ class Simulation:
                 sec_name = entry.section.split(".")[-1]
             else:
                 sec_name = entry.section.split(".")[1] # name[idx]
-            print(f"sec_name: {sec_name}")
+            #print(f"sec_name: {sec_name}")
             seg_sections.append(sec_name.split("[")[0])
             seg_idx.append(sec_name.split("[")[1].split("]")[0])
             seg_coords.append(entry.coords)
@@ -119,134 +119,146 @@ class Simulation:
           elec_distances_nexus = cell.compute_electrotonic_distance(from_segment = cell.apic[36](0.961538))
         elec_distances_nexus.to_csv(os.path.join(parameters.path, "elec_distance_nexus.csv"))
 
-        # Create an ECP object for extracellular potential
-        #elec_pos = params.ELECTRODE_POSITION
-        #ecp = EcpMod(cell, elec_pos, min_distance = params.MIN_DISTANCE)
-        # Set ECP
-        # if parameters.record_ecp == True:
-        #     # Reason: (NEURON: Impedance calculation with extracellular not implemented)
-        #     self.logger.log_warining("Recording ECP adds the extracellular channel to all segments after computing electrotonic distance.\
-        #                              This channel is therefore not accounted for in impedence calculation, but it might affect the simulation.")
-        #     h.cvode.use_fast_imem(1)
-        #     for sec in cell.all: sec.insert('extracellular')
-        #     cell.add_segment_recorders(var_name = "i_membrane_")
-
         # Save constants
         with open(os.path.join(parameters.path, "parameters.pickle"), "wb") as file:
            pickle.dump(parameters, file)
 
-        # In time stamps, i.e., ms / dt
-        time_step = 0
-
-        h.celsius = parameters.h_celcius
-        h.tstop = parameters.h_tstop
-        h.dt = parameters.h_dt
-        h.steps_per_ms = 1 / h.dt
-        if is_indexable(cell.soma):
-            h.v_init = cell.soma[0].e_pas
+        if parameters.simulate_EPSPs:
+          self.simulate_EPSPs(cell, parameters)
         else:
-            h.v_init = cell.soma.e_pas
+          self.set_all_recorders(cell, parameters)
+          self.simulate(cell, parameters)
 
-        h.finitialize(h.v_init)
-
-        self.logger.log("Starting simulation.")
-#        while h.t <= h.tstop + 1:
-#
-#            if (time_step > 0) & (time_step % (parameters.save_every_ms / parameters.h_dt) == 0):
-#                # Log progress
-#                self.logger.log_step(time_step)
-#
-#                # Save data
-#                cell.write_recorder_data(
-#                    os.path.join(parameters.path, f"saved_at_step_{time_step}"), 
-#                    int(1 / parameters.h_dt))
-#                self.logger.log("Finish writing data")
-#                # Save lfp
-#                #loc_param = [0., 0., 45., 0., 1., 0.]
-#                #lfp = ecp.calc_ecp(move_cell = loc_param).T  # Unit: mV
-#
-#                #with h5py.File(os.path.join(parameters.path, f"saved_at_step_{time_step}", "lfp.h5"), 'w') as file:
-#                #    file.create_dataset("report/biophysical/data", data = lfp)
-#
-#                # Save net membrane current
-#                #with h5py.File(os.path.join(parameters.path, f"saved_at_step_{time_step}", "i_membrane_report.h5"), 'w') as file:
-#                #    file.create_dataset("report/biophysical/data", data = ecp.im_rec.as_numpy())
-#
-#                # Reinitialize recording vectors
-#                for recorder_or_list in cell.recorders: recorder_or_list.clear()
-#                self.logger.log_step("Finish clearing recorders")
-#
-#                #for vec in ecp.im_rec.vectors: vec.resize(0)
-#
-#            h.fadvance()
-#            time_step += 1
-#        self.logger.log("Finish simulation")
-#        try:
-#          while h.t <= h.tstop + 1:
-#  
-#              if (time_step > 0) and (time_step % (parameters.save_every_ms / parameters.h_dt) == 0):
-#                  self.logger.log(f"Saving data at step: {time_step}")
-#                  try:
-#                      # Save data
-#                      cell.write_recorder_data(
-#                          os.path.join(parameters.path, f"saved_at_step_{time_step}"), 
-#                          1)#int(1 / parameters.h_dt))
-#                      self.logger.log("Finished writing data")
-#  
-#                      # Reinitialize recording vectors
-#                      for recorder_or_list in cell.recorders: recorder_or_list.clear()
-#                      self.logger.log("Finished clearing recorders")
-#                  except Exception as e:
-#                      self.logger.log(f"Error during data saving or recorder clearing at step {time_step}: {e}")
-#  
-#              try:
-#                  h.fadvance()
-#              except Exception as e:
-#                  self.logger.log(f"Error advancing simulation at time_step {time_step}: {e}")
-#                  break  # Exit the loop on error
-#              
-#              time_step += 1
-#  
-#          self.logger.log("Finish simulation")
-#        except Exception as e:
-#          self.logger.log(f"Unexpected error in run_single_simulation: {e}")    
-
-        #try:
-        start_time = time.time()
-        while h.t <= h.tstop + 1:
-
-            if (time_step > 0) and (time_step % (parameters.save_every_ms / parameters.h_dt) == 0):
-                self.logger.log(f"Saving data at step: {time_step}")
-                #try:
-                # Save data
-                cell.write_recorder_data(
-                    os.path.join(parameters.path, f"saved_at_step_{time_step}"), 
-                    int(1 / parameters.h_dt))
-                self.logger.log("Finished writing data")
-
-                # Reinitialize recording vectors
-                for recorder_or_list in cell.recorders: recorder_or_list.clear()
-                self.logger.log("Finished clearing recorders")
-                #except Exception as e:
-                #    self.logger.log(f"Error during data saving or recorder clearing at step {time_step}: {e}")
-
-            try:
-                h.fadvance()
-            except Exception as e:
-                self.logger.log(f"Error advancing simulation at time_step {time_step}: {e}")
-                break  # Exit the loop on error
+    def set_all_recorders(self, cell, parameters: SimulationParameters):
+        # Set recorders
+        if parameters.record_all_channels:
+            for var_name in parameters.channel_names:
+                cell.add_segment_recorders(var_name = var_name)
+        if parameters.record_all_v:
+            cell.add_segment_recorders(var_name = "v")
+       
+        if parameters.record_ecp == True:
+    			# Create an ECP object for extracellular potential
+    			#elec_pos = params.ELECTRODE_POSITION
+    			#ecp = EcpMod(cell, elec_pos, min_distance = params.MIN_DISTANCE)
+    			#     # Reason: (NEURON: Impedance calculation with extracellular not implemented)
+            self.logger.log_warining("Recording ECP adds the extracellular channel to all segments after computing electrotonic distance.\
+                                      This channel is therefore not accounted for in impedence calculation, but it might affect the simulation.")
+            h.cvode.use_fast_imem(1)
+    			#for sec in cell.all: sec.insert('extracellular') # may not be needed
+            cell.add_segment_recorders(var_name = "i_membrane_")
+    		
+        if (not parameters.all_synapses_off) and (parameters.record_all_synapses):
+            for var_name in ["i_AMPA", "i_NMDA"]:
+                cell.add_synapse_recorders(var_name = var_name)
+        if parameters.record_soma_spikes:
+            cell.add_spike_recorder(sec = cell.soma[0], var_name = "soma_spikes", spike_threshold = parameters.spike_threshold)
+        if parameters.record_axon_spikes:
+            cell.add_spike_recorder(sec = cell.axon[0], var_name = "axon_spikes", spike_threshold = parameters.spike_threshold)
+    
+    # def simulate_EPSPs(self, cell, parameters: SimulationParameters):
+    #         results = []
+    
+    #         # Temporarily disable all synapses to ensure a controlled start
+    #         for synapse in cell.synapses:
+    #             for netcon in synapse.netcons:
+    #                 netcon.active(False)
+    
+    #         # Iterate over each synapse to simulate EPSPs one by one
+    #         for idx, synapse in enumerate(cell.synapses):
+    #             # Reactivate the current synapse with a spike at 50
+    #             spike_train = [50]
+    #             vec = h.Vector(spike_train)
+    #             stim = h.VecStim()
+    #             stim.play(vec)
+    #             synapse.netcons.append(h.NetCon(stim, synapse.h_syn, 1, 0, 1))
+    #             path = os.path.join(parameters.path, f"synapse_{idx}")
+               
+    #             # Record data
+    #             synapse_type = synapse.syn_mod
+    #             segment_name = str(synapse.h_syn.get_segment())
+    #             cell.add_segment_recorders(var_name='v', segment_to_record=cell.soma[0](0.5))
+    #             cell.add_segment_recorders(var_name='v', segment_to_record=synapse.h_syn.get_segment())
+    #             distance = h.distance(synapse.h_syn.get_segment(), cell.soma[0](0.5))
+    #             results.append({"Synapse Index": idx, "Synapse Type": synapse_type,"Segment Name": segment_name, "Distance from Soma": distance})
             
-            time_step += 1
+    #             # Simulate
+    #             self.simulate(cell, parameters, path=parameters.path+str(idx))
+        
+    #             cell.recorders = []
 
-        end_time = time.time()
-        simulation_runtime = end_time - start_time
-        self.logger.log("Finish simulation in {simulation_runtime}")
-        # Record the simulation runtime to a file
-        runtime_file_path = os.path.join(parameters.path, "simulation_runtime.txt")
-        with open(runtime_file_path, "w") as runtime_file:
-            runtime_file.write(f"Simulation runtime: {simulation_runtime} seconds")
-        #except Exception as e:
-        #  self.logger.log(f"Unexpected error in run_single_simulation: {e}") 
+    #             # Deactivate the synapse after simulation
+    #             for netcon in synapse.netcons:
+    #                 netcon.active(False)
+
+    #         # Convert results to a DataFrame
+    #         df_results = pd.DataFrame(results)
+    
+    #         # Output DataFrame to a CSV file or other storage as needed
+    #         df_results.to_csv(os.path.join(path, 'EPSP_simulation_results.csv'), index=False)
+    
+    #         return df_results
+          
+        
+      
+    def simulate(self, cell, parameters: SimulationParameters, log=True, record_runtime=True, path=None):
+            if path is None:
+                path = parameters.path
+                
+            if not os.path.exists(path):
+                os.mkdir(path)
+
+            # In time stamps, i.e., ms / dt
+            time_step = 0
+    
+            h.celsius = parameters.h_celcius
+            h.tstop = parameters.h_tstop
+            h.dt = parameters.h_dt
+            h.steps_per_ms = 1 / h.dt
+            if is_indexable(cell.soma):
+                h.v_init = cell.soma[0].e_pas
+            else:
+                h.v_init = cell.soma.e_pas
+    
+            h.finitialize(h.v_init)
+    
+            if log: self.logger.log("Starting simulation.")
+    
+            if record_runtime: start_time = time.time()
+            while h.t <= h.tstop + 1:
+    
+                if (time_step > 0) and (time_step % (parameters.save_every_ms / parameters.h_dt) == 0):
+                    self.logger.log(f"Saving data at step: {time_step}")
+    
+                    # Save data
+                    cell.write_recorder_data(
+                        os.path.join(path, f"saved_at_step_{time_step}"), 
+                        int(1 / parameters.h_dt))
+                    if log: self.logger.log("Finished writing data")
+    
+                    # Reinitialize recording vectors
+                    for recorder_or_list in cell.recorders: recorder_or_list.clear()
+                    if log: self.logger.log("Finished clearing recorders")
+    
+                try:
+                    h.fadvance()
+                except Exception as e:
+                    self.logger.log(f"Error advancing simulation at time_step {time_step}: {e}")
+                    break  # Exit the loop on error
+                
+                time_step += 1
+    
+            if record_runtime: 
+              end_time = time.time()
+              simulation_runtime = end_time - start_time
+            if log: self.logger.log(f"Finish simulation in {simulation_runtime:.3f} seconds")
+            # Record the simulation runtime to a file
+            if record_runtime:
+              runtime_file_path = os.path.join(path, "simulation_runtime.txt")
+              with open(runtime_file_path, "w") as runtime_file:
+                  runtime_file.write(f"Simulation runtime: {simulation_runtime:.3f} seconds")
+            #except Exception as e:
+            #  self.logger.log(f"Unexpected error in run_single_simulation: {e}")
 
 def is_indexable(obj: object):
     """
