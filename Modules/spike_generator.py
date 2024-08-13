7f518ecc6ab37208cc4c6dfe970b43965507020e
davidfague <davidfague@live.com>
2024-08-13 12:11:53 -0500

    implement delayed inhibition from exc spike trains. usage was commited in the clustering update.

--- Modules/spike_generator.py
+++ Modules/spike_generator.py
@@ -16,6 +16,22 @@ class SpikeTrain:
 
 class PoissonTrainGenerator:
 	# Win_size = 1 ms
+	# labmdas refers to an array of firing_rates over time
+ 
+	@staticmethod
+	def generate_lambdas_by_delaying(num: int,
+			spike_trains: list) -> np.ndarray:
+			# num is the number of samples
+  
+		# Get lambdas
+		lambdas = PoissonTrainGenerator.generate_delayed_fr_profile(num=num, spike_trains = spike_trains)
+
+		# # Can't have negative firing rates (precision reasons)
+		# if np.sum(lambdas < 0) != 0:
+		# 	warnings.warn("Found non-positive lambdas when generating a spike train.")
+		# 	lambdas[lambdas < 0] = 0 
+		
+		return lambdas
 
 	@staticmethod
 	def generate_lambdas_from_pink_noise(
@@ -31,10 +47,10 @@ class PoissonTrainGenerator:
 		if rhythmic_modulation:
 			lambdas = PoissonTrainGenerator.rhythmic_modulation(lambdas)
 		
-		# Can't have negative firing rates (precision reasons)
-		if np.sum(lambdas < 0) != 0:
-			warnings.warn("Found non-positive lambdas when generating a spike train.")
-			lambdas[lambdas < 0] = 0 
+		# # Can't have negative firing rates (precision reasons)
+		# if np.sum(lambdas < 0) != 0:
+		# 	warnings.warn("Found non-positive lambdas when generating a spike train.")
+		# 	lambdas[lambdas < 0] = 0 
 		
 		return lambdas
 	
@@ -79,15 +95,78 @@ class PoissonTrainGenerator:
 		white_noise = random_state.normal(loc = mean, scale = std, size = num_obs + 2000)
 
 		# Apply the FIR/IIR filter to create the 1/f noise, minmax and shift to bounds
-		fr_profile = minmax(lfilter(B, A, white_noise)[2000:]) * (bounds[1] - bounds[0]) + bounds[0]
+		fr_profile = minmax(lfilter(B, A, white_noise)[2000:])# * (bounds[1] - bounds[0]) + bounds[0]
 
 		return fr_profile
 	
 	@staticmethod
-	def delay_spike_trains(
-			spike_trains_to_delay: list,
-			shift: int = 4) -> list:
-		
+	def generate_delayed_fr_profile(num, spike_trains):
+		'''trains_matrix is n_pre_cells x n_timepoints'''
+		# all of the following are 1x n _timepoints
+		old_spike_probs = PoissonTrainGenerator.compute_probs_from_spike_trains(num, spike_trains)
+		new_spike_probs = PoissonTrainGenerator.shift_wrap_array(old_spike_probs, 4) # delay the exc spike train
+		new_frs = PoissonTrainGenerator.compute_frs_from_probs(new_spike_probs)
+		return new_frs
+
+	@staticmethod
+	def compute_probs_from_spike_trains(num, spike_trains):
+		# Assuming exc_spike_trains is a list of lists containing spike times for each synapse
+		nsyn = len(spike_trains)
+		# Initialize an array to count spikes at each time point
+		spike_counts = np.zeros(num)
+		# Count spikes at each time point
+		for train in spike_trains:
+			spike_counts[train] += 1
+		# Calculate the mean spike train
+		mean_spike_train = spike_counts / nsyn # mean_spike_train[0] is the probability of observing a spike in a train at t[0]
+  
+		# Check if any probabilities are greater than 1 and get their indices and values
+		indices = np.where(mean_spike_train > 1)[0]
+		values = mean_spike_train[indices]
+		if len(indices) > 0:
+			print(f"Warning: Probabilities greater than 1 detected at indices {indices} with values {values}")
+
+		return mean_spike_train
+
+	@staticmethod
+	def shift_wrap_array(arr, by):
+		wrap = arr[-by:].copy()
+		arr[by:] = arr[0:-by]
+		arr[0:by] = wrap
+		return arr
+
+	@staticmethod
+	def compute_frs_from_probs(spike_probs):
+		'''in units mHz'''
+		# Clamp spike_probs to avoid 0 and 1
+		epsilon = 1e-15
+		spike_probs = np.clip(spike_probs, epsilon, 1 - epsilon)
+		return -np.log(1 - spike_probs)
+
+	@staticmethod
+	def shift_mean_of_lambdas(lambdas, desired_mean):
+		'''frs: np.array of firing rates that will be lamda
+		units mHz'''
+  		# Can't have negative firing rates (precision reasons)
+		if np.sum(lambdas < 0) != 0:
+			warnings.warn("Found non-positive lambdas before shifing a mean.")
+			lambdas[lambdas < 0] = 1e-15 
+		# print(np.sum(lambdas < 0),lambdas)
+		# if divide_1000:
+		# 	desired_mean = desired_mean / 1000 # shift
+		lambdas = lambdas + (desired_mean - np.mean(lambdas))
+
+		# print(np.sum(lambdas < 0),lambdas)
+  
+  		# Can't have negative firing rates (precision reasons)
+		if np.sum(lambdas < 0) != 0:
+			warnings.warn("Found non-positive lambdas after shifing a mean.")
+			lambdas[lambdas < 0] = 1e-15 
+   
+		return lambdas
+
+	@staticmethod
+	def delay_spike_trains(spike_trains_to_delay: list, shift: int = 4) -> list:
 		delayed_trains = []
 		for train in spike_trains_to_delay:
 			delayed_times = train.spike_times - shift
@@ -113,5 +192,57 @@ class PoissonTrainGenerator:
 			num_points = random_state.poisson(lambd / 1000)
 			if num_points > 0: t[i] = 1
 
+		# print(np.mean(t)*1000)
+
 		spike_times = np.where(t > 0)[0]
-		return SpikeTrain(spike_times, len(lambdas))
+		return SpikeTrain(spike_times, len(lambdas), np.mean(t)*1000)
+
+	# @staticmethod
+	# def delay_modulation(spike_trains_to_delay: list, fr_time_shift: int, spike_train_t: int, 
+	# 		  			 spike_train_dt: float = 1e-3, bounds = (0, 2)) -> np.ndarray:
+	# 	'''
+	# 	Compute a firing rate profile from a target spike train and shift it to create a new profile.
+
+	# 	Parameters:
+	# 	----------
+	# 	spike_trains_to_delay: list
+	# 		Target spike trains.
+
+	# 	fr_time_shift: int
+	# 		Shift of the target spike train's rate profile.
+
+	# 	spike_train_t: int
+	# 		Time length that was used to generate spike_trains_to_delay.
+
+	# 	spike_train_dt: float
+	# 		Time discretization that was used to generate spike_trains_to_delay.
+
+	# 	bounds: tuple
+	# 		The profile bounds: [min, max]. 
+
+	# 	Returns:
+	# 	----------
+	# 	fr_profile: np.ndarray
+	# 		Firing rate profile.
+
+	# 	'''
+	# 	if fr_time_shift is None:
+	# 		raise TypeError('fr_time_shift must be an integer.')
+   
+	# 	# Flatten all the spike trains, because otherwise np.histogram doesn't work
+	# 	# Ignore spike trains without spikes
+	# 	times_where_spikes = [sp_time for sp_train in spike_trains_to_delay for sp_time in sp_train if len(sp_train) > 0]
+	# 	# Compute the firing rate profile from the histogram
+	# 	hist, _ = np.histogram(times_where_spikes, bins = spike_train_t)
+
+	# 	fr_profile = hist / (spike_train_dt * (len(spike_trains_to_delay) + 1))
+
+	# 	# Shift by fr_time_shift
+	# 	wrap = fr_profile[-fr_time_shift:]
+	# 	fr_profile[fr_time_shift:] = fr_profile[0:-fr_time_shift]
+	# 	fr_profile[0:fr_time_shift] = wrap
+
+	# 	# Minmax and shift to bounds
+	# 	fr_profile = minmax(fr_profile) * (bounds[1] - bounds[0]) + bounds[0]
+
+	# 	return fr_profile
