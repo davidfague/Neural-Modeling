d73268329910736292005c29ee16fcc4dd9a1c5d
davidfague <davidfague@live.com>
2024-12-06 12:27:30 -0600

    not meaningful update to scripts/sim.py

--- scripts/sim.py
+++ scripts/sim.py
@@ -9,6 +9,7 @@ from Modules.constants import HayParameters
 if __name__ == "__main__":
     
     sim = Simulation(SkeletonCell.Hay)
-    sim.submit_job(HayParameters("try1", h_tstop = 2000, reduce_cell = False, expand_cable = False))
+    #sim.submit_job(HayParameters("Expanded_LFP_try1", h_tstop= 5000, reduce_cell = True, expand_cable = True))
+    sim.submit_job(HayParameters(f"selective", h_tstop= 1000, reduce_cell = True, reduce_cell_selective = True, record_ecp=True, reduce_tufts=True))
     sim.run()
 
