77d0c0e003ebc027fece825f89f849ccbb7fa16c
Vladimir Omelyusik <vsomelyusik@gmail.com>
2025-01-22 15:49:36 -0600

    change slurm to limit memory per cpu

--- scripts/slurm_sim_batch.sh
+++ scripts/slurm_sim_batch.sh
@@ -3,6 +3,8 @@
 #SBATCH --job-name=my_awesome_sim_with_lots_of_cells_AAAA
 #SBATCH -N 1
 #SBATCH -n 2
-#SBATCH --mem=200G
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=5G
 
 mpiexec python slurm_sim.py
