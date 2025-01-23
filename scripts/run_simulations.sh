173b18678d60323fe17a9580127b3b3e36b41c54
davidfague <davidfague@live.com>
2025-01-23 15:15:41 -0600

    update run_simulations.sh. slurm_sim_batch.sh is the alternative now.

--- scripts/run_simulations.sh
+++ scripts/run_simulations.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-#SBATCH -J  M1_build
-#SBATCH -o  M1_build.out
-#SBATCH -e  M1_build.error
+#SBATCH -J  single_cell
+#SBATCH -o  single_cell.out
+#SBATCH -e  single_cell.error
 #SBATCH -t 0-48:00:00  # days-hours:minutes
 
 #SBATCH -N 1
 #SBATCH -n 1 # used for MPI codes, otherwise leave at '1'
-##SBATCH --ntasks-per-node=1  # don't trust SLURM to divide the cores evenly
-##SBATCH --cpus-per-task=1  # cores per task; set to one if using MPI
+#SBATCH --ntasks-per-node=1  # don't trust SLURM to divide the cores evenly
+#SBATCH --cpus-per-task=1  # cores per task; set to one if using MPI
 ##SBATCH --exclusive  # using MPI with 90+% of the cores you should go exclusive
-#SBATCH --mem-per-cpu=1G  # memory per core; default is 1GB/core
+#SBATCH --mem-per-cpu=5G  # memory per core; default is 1GB/core
 
 
 START=$(date)
