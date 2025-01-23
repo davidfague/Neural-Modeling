30395c013097f5b07fad1500b2a028f34ef69781
davidfague <davidfague@live.com>
2025-01-23 15:18:08 -0600

    rename job in slurm_sim_batch.sh and increase mem per cpu.

--- scripts/slurm_sim_batch.sh
+++ scripts/slurm_sim_batch.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
   
-#SBATCH --job-name=my_awesome_sim_with_lots_of_cells_AAAA
+#SBATCH --job-name=testing
 #SBATCH -N 1
-#SBATCH -n 2
+#SBATCH -n 5
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=5G
+#SBATCH --cpus-per-task=1 # cores per task; set to one if using MPI
+#SBATCH --mem-per-cpu=5G # memory per core; default is 1GB/core
 
 mpiexec python slurm_sim.py
