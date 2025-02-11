b22e297bc9ef9f51a3c0d4bd6893df27d3aaab7a
davidfague <davidfague@live.com>
2025-02-11 11:07:49 -0600

    in 'slurm_sim_batch.sh' pass 'sim_name' to 'slurm_sim.py' to limit number of files that must be visited when designing new simulation

--- scripts/slurm_sim.py
+++ scripts/slurm_sim.py
@@ -12,11 +12,15 @@ from Modules.cell_builder import SkeletonCell
 from neuron import h
 import pickle
 
-sim_title = 'BenSynapses_testing_depth_of_mod_more_seeds'
+# Use a command-line argument for sim_title if provided, otherwise use a default value.
+if len(sys.argv) > 1:
+    sim_title = sys.argv[1]
+else:
+    sim_title = 'sim'  # Provide a default if no argument is given.
 
 if __name__ == "__main__":
 
-    print(rank)
+    print(f"Rank {rank} running simulation with title: {sim_title}")
 
     try:
         h.load_file('stdrun.hoc')
--- scripts/slurm_sim_batch.sh
+++ scripts/slurm_sim_batch.sh
@@ -2,9 +2,13 @@
   
 #SBATCH --job-name=testing
 #SBATCH -N 1
-#SBATCH -n 5
-#SBATCH --ntasks-per-node=1
+#SBATCH -n 1
+#SBATCH --ntasks-per-node=9
 #SBATCH --cpus-per-task=1 # cores per task; set to one if using MPI
 #SBATCH --mem-per-cpu=5G # memory per core; default is 1GB/core
 
-mpiexec python slurm_sim.py
+# Set the simulation title here
+SIM_TITLE="testing_new_weights"
+
+# Pass SIM_TITLE as a command-line argument to the Python script
+mpiexec python slurm_sim.py "$SIM_TITLE"
