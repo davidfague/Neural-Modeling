5a83425fdb65be1fe7e346c3e7592255c1f8f3d8
davidfague <davidfague@live.com>
2025-02-14 12:10:17 -0600

    fix slurm usage. add note to adjust n

--- scripts/slurm_sim_batch.sh
+++ scripts/slurm_sim_batch.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
   
-#SBATCH --job-name=testing
+#SBATCH --job-name=SingleCellStudy
 #SBATCH -N 1
-#SBATCH -n 1
-#SBATCH --ntasks-per-node=9
+#SBATCH -n 20 # change this based on N_workers
+#SBATCH --ntasks-per-node=20 # change this based on N_workers
 #SBATCH --cpus-per-task=1 # cores per task; set to one if using MPI
 #SBATCH --mem-per-cpu=5G # memory per core; default is 1GB/core
+#SBATCH --time=12:00:00  # Set a reasonable max runtime
 
 # Set the simulation title here
-SIM_TITLE="testing_new_weights"
+SIM_TITLE="measuring_across_rhyths_across_seeds"
 
 # Pass SIM_TITLE as a command-line argument to the Python script
 mpiexec python slurm_sim.py "$SIM_TITLE"
