322174bd3868ee3f53341af1f9dcc9c6d2ef2f71
davidfague <davidfague@live.com>
2025-03-14 12:53:34 -0500

    update group_simulations_by_parameter

--- Modules/group_simulations.py
+++ Modules/group_simulations.py
@@ -8,24 +8,25 @@ import numpy as np
 def update_parameters_to_include_morphology_name(simulations_to_parameters): #@DEPRACATING
     # in the near future this will be unnecessary.
     # Warning("Deprecating update_parameters_to_include_morphology_name")
-    for sim_folder, parameters in simulations_to_parameters.items():
-        replace = False
-        if not hasattr(parameters, 'morphology_name'):
-            replace=True
-        elif getattr(parameters, 'morphology_name') == '' or getattr(parameters, 'morphology_name') == 'no_morphology_name_provided':
-            replace=True
-        if replace:
-            if 'complex' in sim_folder.lower():
-                best_guess = 'Complex'
-            elif 'branches' in sim_folder.lower():
-                best_guess = 'Branches'
-            elif 'trees' in sim_folder.lower():
-                best_guess = 'Trees'
-            else:
-                Warning("No morphology_name best_guess for {sim_folder}")
-            parameters.morphology_name = best_guess
-        else:
-            print(Warning("Time to fully Deprecate update_parameters_to_include_morphology_name"))
+    # for sim_folder, parameters in simulations_to_parameters.items():
+    #     replace = False
+    #     if not hasattr(parameters, 'morphology_name'):
+    #         replace=True
+    #     elif getattr(parameters, 'morphology_name') == '' or getattr(parameters, 'morphology_name') == 'no_morphology_name_provided':
+    #         replace=True
+    #     if replace:
+    #         if 'complex' in sim_folder.lower():
+    #             best_guess = 'Complex'
+    #         elif 'branches' in sim_folder.lower():
+    #             best_guess = 'Branches'
+    #         elif 'trees' in sim_folder.lower():
+    #             best_guess = 'Trees'
+    #         else:
+    #             Warning("No morphology_name best_guess for {sim_folder}")
+    #         parameters.morphology_name = best_guess
+    #     else:
+    #         print(Warning("Time to fully Deprecate update_parameters_to_include_morphology_name"))
+    # return simulations_to_parameters
     return simulations_to_parameters
 
 def get_parameters_for_simulations(simulations_folder, return_full_path_keys=False):
@@ -43,13 +44,12 @@ def get_parameters_for_simulations(simulations_folder, return_full_path_keys=Fal
 
 def group_simulations_by_parameter(simulations_folder, parameter_name, return_simulations_to_parameters = False):
     simulations_to_parameters = get_parameters_for_simulations(simulations_folder)
-    # print(f"simulations_to_parameters: {simulations_to_parameters}")
-    # print(f"simulations_to_parameters: {simulations_to_parameters}")
     grouped_simulations_by_parameter = {}
     for sim_folder_name in simulations_to_parameters.keys():
         parameter_value = getattr(simulations_to_parameters[sim_folder_name], parameter_name)
-        if not hasattr(grouped_simulations_by_parameter, parameter_value):
+        if not parameter_value in grouped_simulations_by_parameter.keys():
             grouped_simulations_by_parameter[str(parameter_value)] = []
+
         grouped_simulations_by_parameter[str(parameter_value)].append(sim_folder_name)
     if return_simulations_to_parameters:
         return grouped_simulations_by_parameter, simulations_to_parameters
