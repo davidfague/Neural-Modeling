6b37579410de95399aa5c1bcd0d51543e45fad24
davidfague <davidfague@live.com>
2025-10-20 16:55:12 -0500

    upload branching_coords_for_clusters/

---
+++ terminal_branching_coords_for_clusters/branch_statistics.py
@@ -0,0 +1,90 @@
+branch_statistics = {
+    2016: {
+        'total_length': 497.83,
+        'center_coords': (-54.61, 983.56, -62.58)
+    },
+    2179: {
+        'total_length': 362.37,
+        'center_coords': (12.45, 1030.49, -20.27)
+    },
+    2086: {
+        'total_length': 187.82,
+        'center_coords': (-35.70, 1101.04, -89.13)
+    },
+    1706: {
+        'total_length': 475.26,
+        'center_coords': (-64.42, 955.42, -10.51)
+    },
+    2259: {
+        'total_length': 245.84,
+        'center_coords': (20.75, 1068.68, -21.08)
+    },
+    2230: {
+        'total_length': 132.06,
+        'center_coords': (26.62, 1115.52, -29.18)
+    },
+    2047: {
+        'total_length': 123.04,
+        'center_coords': (-67.02, 1098.08, -25.56)
+    },
+    2165: {
+        'total_length': 30.96,
+        'center_coords': (36.60, 1135.26, -11.40)
+    },
+    2041: {
+        'total_length': 12.28,
+        'center_coords': (-63.12, 1129.39, -1.74)
+    },
+    1684: {
+        'total_length': 46.53,
+        'center_coords': (-123.92, 1096.61, 3.70)
+    },
+    1689: {
+        'total_length': 19.55,
+        'center_coords': (-114.04, 1098.55, 7.15)
+    },
+    2145: {
+        'total_length': 42.96,
+        'center_coords': (23.83, 1088.20, 6.62)
+    },
+    2283: {
+        'total_length': 114.60,
+        'center_coords': (42.28, 1023.40, 4.12)
+    },
+    1890: {
+        'total_length': 316.03,
+        'center_coords': (41.02, 910.67, -23.24)
+    },
+    1892: {
+        'total_length': 4.16,
+        'center_coords': (71.60, 1032.22, 14.60)
+    },
+    1883: {
+        'total_length': 18.10,
+        'center_coords': (77.80, 1025.09, 8.70)
+    },
+    1880: {
+        'total_length': 3.61,
+        'center_coords': (77.18, 1027.83, 10.34)
+    },
+    1757: {
+        'total_length': 194.30,
+        'center_coords': (-11.39, 926.71, -4.74)
+    },
+    1631: {
+        'total_length': 44.05,
+        'center_coords': (-57.44, 931.19, 4.16)
+    },
+    1732: {
+        'total_length': 28.10,
+        'center_coords': (-24.60, 921.82, 5.49)
+    },
+    1729: {
+        'total_length': 3.10,
+        'center_coords': (-22.55, 926.13, 8.99)
+    },
+    1837: {
+        'total_length': 108.96,
+        'center_coords': (16.59, 900.80, -13.82)
+    }
+} 
---
+++ terminal_branching_coords_for_clusters/branch_statistics_combined.py
@@ -0,0 +1,46 @@
+combined_branch_statistics = {
+    2016: {
+        'total_length': 497.83,
+        'center_coords': (-54.61, 983.56, -62.58)
+    },
+    2179: {
+        'total_length': 362.37,
+        'center_coords': (12.45, 1030.49, -20.27)
+    },
+    2086: {
+        'total_length': 187.82,
+        'center_coords': (-35.70, 1101.04, -89.13)
+    },
+    1706: {
+        'total_length': 519.31,
+        'center_coords': (-63.82, 953.36, -9.27)
+    },
+    2259: {
+        'total_length': 288.81,
+        'center_coords': (21.21, 1071.59, -16.96)
+    },
+    2230: {
+        'total_length': 163.02,
+        'center_coords': (28.52, 1119.27, -25.80)
+    },
+    2047: {
+        'total_length': 154.87,
+        'center_coords': (-72.64, 1100.62, -19.54)
+    },
+    2283: {
+        'total_length': 118.21,
+        'center_coords': (43.34, 1023.53, 4.31)
+    },
+    1890: {
+        'total_length': 316.03,
+        'center_coords': (41.02, 910.67, -23.24)
+    },
+    1757: {
+        'total_length': 197.40,
+        'center_coords': (-11.57, 926.70, -4.53)
+    },
+    1837: {
+        'total_length': 108.96,
+        'center_coords': (16.59, 900.80, -13.82)
+    },
+}
