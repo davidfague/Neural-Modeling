31420f2160ebef4d71e74f06e0d295c0547cf3e9
davidfague <davidfague@live.com>
2025-11-13 14:57:00 -0600

    update scripts/AA_pre_sim.py logging and configure_sim_params path.

--- scripts/AA_pre_sim.py
+++ scripts/AA_pre_sim.py
@@ -16,7 +16,7 @@ import copy
 import Modules.analysis as analysis
 
 from Modules.constants import HayParameters
-from scripts.configure_sim_params import configure_sim_params
+from configure_sim_params import configure_sim_params
 from Modules.simulation_slurm import Simulator
 from Modules.segments_file import generate_segments_csv
 from Modules.synapses_file import (
@@ -26,6 +26,7 @@ from Modules.synapses_file import (
 )
 from Modules.synapse_analysis import SynapseAnalyzer
 from Modules.plot_morphology import plot_morphology_flex, plot_reduced_morphology
+from Modules.logger import Logger
 
 # ---------------- basics ----------------
 SIMULATIONS_FOLDER = "../simulations"  # added leading slash
@@ -94,6 +95,7 @@ def plot_morphology(sim_dir):
     ### each sec_type
     seg_data = pd.read_csv(os.path.join(sim_dir, "segment_data.csv"))
     parameters = analysis.DataReader.load_parameters(sim_dir) # load parameters
+    logger = Logger(sim_dir)
     figs, axs = plot_morphology_flex(
             seg_data,
             option='each_sec_type',
@@ -103,6 +105,7 @@ def plot_morphology(sim_dir):
             show=True,          # set True if you want interactive windows
             save=True,           # saves PNGs named <sec_type>.png
             color='red',         # all types get same color; can change
+            logger=logger,
         )
 
 
@@ -128,22 +131,22 @@ def run_pre_sim():
     )
     simulator.create_simulation_folders()
 
-    write_spike_train_modes(simulator.sims_dir, inh_syn_properties, exc_syn_properties)
-
-    # Run generators across all sims
-    log(f"\n[AA_pre_sim] Generating segments CSV on all sims in: {simulator.sims_dir}\n")
-    simulator.run_on_all_sims_parallel(simulator.sims_dir, generate_segments_csv)
-    log(f"\n[AA_pre_sim] Plotting morphology on all sims in: {simulator.sims_dir}\n")
-    plot_morphology(sim_dir)
-    log(f"\n[AA_pre_sim] Running synapse generation on all sims in: {simulator.sims_dir}\n")
-    simulator.run_on_all_sims_parallel(simulator.sims_dir, use_pssg)
-
     # Build a list of full sim paths
     sim_dirs = [
         os.path.join(simulator.sims_dir, d)
         for d in os.listdir(simulator.sims_dir)
         if os.path.isdir(os.path.join(simulator.sims_dir, d))
     ]
+
+    write_spike_train_modes(simulator.sims_dir, inh_syn_properties, exc_syn_properties)
+
+    # Run generators across all sims
+    log(f"\n[AA_pre_sim] Generating segments CSV on all sims in: {simulator.sims_dir}\n")
+    simulator.run_on_all_sims_parallel(simulator.sims_dir, generate_segments_csv)
+    log(f"\n[AA_pre_sim] Plotting morphology on all sims in: {simulator.sims_dir}\n")
+    simulator.run_on_all_sims_parallel(simulator.sims_dir, plot_morphology)
+    log(f"\n[AA_pre_sim] Running synapse generation on all sims in: {simulator.sims_dir}\n")
+    simulator.run_on_all_sims_parallel(simulator.sims_dir, use_pssg)
 
     # Update spike trains for each sim
     if N_bg_synapses > 0:
