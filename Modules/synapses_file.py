c30d49e4f903d28248668daa7aa44cda0556b1f5
davidfague <davidfague@live.com>
2025-11-13 12:44:12 -0600

    make build_synapses_with_specs error more descriptive

--- Modules/synapses_file.py
+++ Modules/synapses_file.py
@@ -234,7 +234,7 @@ class PreSimSynapseGenerator:
         )
 
         if segments_to_generate_on['probability'].sum() < 0.999 or segments_to_generate_on['probability'].sum() > 1.001:
-            raise ValueError(f"Probabilities do not sum to 1 instead {segments_to_generate_on['probability'].sum()}. Check your segment measurement for probabilities.\nsegments_to_generate_on: {segments_to_generate_on}")
+            raise ValueError(f"Probabilities do not sum to 1 instead {segments_to_generate_on['probability'].sum()} for sec_type {sec_type}. Check your segment measurement for probabilities.\nsegments_to_generate_on: {segments_to_generate_on}")
 
         if use_density:
             # calculate number of synapses per segment
